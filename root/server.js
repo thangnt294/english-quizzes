14ee75f1978b2271d499d1ce7a4224b7fc94ac69
Thang Nguyen <thang290495@gmail.com>
2019-09-20 16:14:24 +0700

    fix

--- root/server.js
+++ root/server.js
@@ -9,11 +9,11 @@ const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 // Routing
 app.use('/questions', require('./routes/questions'))
 app.use('/', require('./routes/auth'))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 // Connect To MongoDB
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
@@ -22,8 +22,8 @@ mongoose.connection.once('open', () => console.log('Connected to the database su
 mongoose.set('useFindAndModify', false)
 
 // Running Server
-const PORT = process.env.PORT || 5000
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
